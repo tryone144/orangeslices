69e87f8d17f7d7a01a7ecb101ebb93e92fafd2a5
Bernd Busse <bernd@busse-net.de>
2016-05-25 21:24:38 +0200

    Added default (testing) example

---
+++ examples/default.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# © 2016 Bernd Busse, The MIT License
+#
+
+import orangeslices as osl
+
+
+def main():
+    orange = osl.Orange()
+
+
+if __name__ == '__main__':
+    main()
