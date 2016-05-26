7ef88b7833dc025912f4e5320d62698bed37f38b
Bernd Busse <bernd@busse-net.de>
2016-05-26 20:26:11 +0200

    updated test example

--- examples/default.py
+++ examples/default.py
@@ -10,6 +10,15 @@ import orangeslices as osl
 def main():
     orange = osl.Orange()
 
+    sep1 = osl.slices.Separator()
+    sep2 = osl.slices.Separator('|')
+    clock = osl.slices.Clock(color_fg="#FF02F3DD", align=osl.ALIGN_LEFT)
+    orange.add(sep1)
+    orange.add(clock)
+    orange.add(sep2)
+
+    orange.run()
+
 
 if __name__ == '__main__':
     main()
